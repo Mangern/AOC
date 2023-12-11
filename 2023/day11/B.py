9d6d22ac45daedf1dbb06ac487c51044f2f9e21f
Magnus <magnuseifr@gmail.com>
2023-12-11 12:20:56 +0100

    Day 11 2023

---
+++ 2023/day11/B.py
@@ -0,0 +1,36 @@
+from itertools import product
+import sys
+
+grid = open(sys.argv[1]).read().splitlines()
+
+erow, ecol = set(), set()
+n = len(grid)
+m = len(grid[0])
+
+for i, row in enumerate(grid):
+    if all(c == '.' for c in row):
+        erow.add(i)
+
+for j in range(m):
+    if all(grid[i][j] == '.' for i in range(n)):
+        ecol.add(j)
+
+pos = []
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == '#':
+            pos.append((i, j))
+
+ans = 0
+for (x1, y1), (x2, y2) in product(pos, pos):
+    if (x1, y1) == (x2, y2):
+        continue
+    ans += abs(x2 - x1) + abs(y2 - y1)
+    for z in erow:
+        if min(x1,x2) < z < max(x1, x2):
+            ans += 10**6-1
+    for z in ecol:
+        if min(y1,y2) < z < max(y1, y2):
+            ans += 10**6-1
+
+print(ans >> 1)
